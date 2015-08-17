1cea872175660f5db140bccfffa5553569f9e1f5
finalspace <finalspace.1031@gmail.com>
2015-08-17 15:51:41 -0400

    Update combinationSumII.cpp

--- algorithms/combinationSumII.cpp
+++ algorithms/combinationSumII.cpp
@@ -1,27 +1,14 @@
-// Source : https://leetcode.com/problems/combination-sum-ii/
+// Source : https://leetcode.com/problems/permutations-ii/
 // Author : Siyuan Xu
-// Date   : 2015-08-11
+// Date   : 2014-08-11
 
 /********************************************************************************** 
 * 
-* Given a collection of candidate numbers (C) and a target number (T), find all 
-* unique combinations in C where the candidate numbers sums to T.
+* Given a collection of numbers that might contain duplicates, return all possible unique permutations.
 * 
-* Each number in C may only be used once in the combination.
-* 
-* Note:
-* 
-* > All numbers (including target) will be positive integers.
-* > Elements in a combination (a1, a2, … , ak) must be in non-descending order. 
-*   (ie, a1 ≤ a2 ≤ … ≤ ak).
-* > The solution set must not contain duplicate combinations.
-* 
-* For example, given candidate set 10,1,2,7,6,1,5 and target 8, 
-* A solution set is: 
-* [1, 7] 
-* [1, 2, 5] 
-* [2, 6] 
-* [1, 1, 6] 
+* For example,
+* [1,1,2] have the following unique permutations:
+* [1,1,2], [1,2,1], and [2,1,1].
 * 
 *               
 **********************************************************************************/
